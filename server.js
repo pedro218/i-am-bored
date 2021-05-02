c476e449d8118838e367ff135fceb9f31c407560
pedro218 <pedroaf218@gmail.com>
2021-05-02 13:05:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const history = require('connect-history-api-fallback')
 const enforce = require('express-sslify')
+const path = require('path')
 
 const app = express()
 
 // Use https instead of http
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
 // Serve our file from where ever the directory it is in
-app.use(serveStatic(__dirname + '/dist'))
+app.use('/', serveStatic(path.join(__dirname, '/dist'))
 
 app.use(history())
 
 // Listen to Heroku's enviremnt varible or to port 50000
-app.listen(process.env.Port || 5000)
-
-
+app.listen(process.env.Port || 8080)
